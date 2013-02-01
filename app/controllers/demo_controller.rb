430d4dfc3da80ed846df064ef4d6716404718b45
KrabDEV <krabdev@icloud.com>
2013-02-01 20:27:43 +0200

    Read Me added

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -7,7 +7,7 @@ class DemoController < ApplicationController
   def hello
    @array = [1,2,3,4,5, "a", "a8"]
    @some_var = "Andreas"
-   @link = {:link => "http://www.google.com", :text => "google"}
+
   end
 
 end
